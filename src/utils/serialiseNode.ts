13c1e1b096eafcbc959bea66798beb798f7df4e4
Sam Haakman <sam@josephmark.studio>
2024-07-25 17:25:16 +1000

    retrieve specific keys when passing node from figma process to plugin

--- src/utils/serialiseNode.ts
+++ src/utils/serialiseNode.ts
@@ -1,6 +1,7 @@
+const WANTED_KEYS = ['id', 'effects', 'cornerRadius', 'fillStyleId', 'fills', 'isAsset', 'name', 'strokes'];
+
 export default function serialiseNode(node: SceneNode) {
-  // TODO: Extract important values
-  return node;
+  return Object.fromEntries(WANTED_KEYS.map((key) => [key, node[key]]));
 }
 
 export function serialiseNodes(nodes: readonly SceneNode[]) {
